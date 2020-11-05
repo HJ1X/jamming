7ced77a24df240ec8a6538c7f1154f2842c35f4f
HJ1X <54548986+HJ1X@users.noreply.github.com>
2020-11-05 18:24:42 +0530

    add app component

---
+++ src/Components/App/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div>
+            <h1>Ja<span className="highlight">mmm</span>ing</h1>
+            <div className="App">
+                <SearchBar />
+                <div className="App-playlist">
+                    <SearchResults />
+                    <Playlist />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
